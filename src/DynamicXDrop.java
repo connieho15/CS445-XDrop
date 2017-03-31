d3b9ad09e11715b86dbbabb705bbd3422303dcdf
reneelau <reneel4u@gmail.com>
2017-03-30 21:33:25 -0700

    added MRSA mecA gene sequences

--- src/DynamicXDrop.java
+++ src/DynamicXDrop.java
@@ -10,17 +10,34 @@ public class DynamicXDrop {
 		double k = 0;
 		double L = 0;
 		double U = 0; 
-		double X = 10;
+		double X = 3;
 		
-		double mat = +3;
+		double mat = 3;
 		double mis = -2;
 		double ind = mis - (mat/2);
 		double half = 0.5;
 		
-		String a = "ABC";
-		String b = "ABC";
-		int N = a.length();
-		int M = b.length();
+		// MRSA strain MSTB8 mecA gene sequence
+		String MSTB8 = "tggggtggttacaacgttacaagatatgaagtggtaaatggtaatatcgacttaa"
+				+ "aacaagcaatagaatcatcagataacattttctttgctagagtagcactcgaatta"
+				+ "ggcagtaagaaatttgaaaaaggcatgaaaaaactaggtgttggtgaagatatacc"
+				+ "aagtgattatccattttataatgctcaaatttcaaacaaaaatttagataatgaaa"
+				+ "tattattagctgattcaggttacggacaaggtgaaatactgattaacccagtacag"
+				+ "atcctttcaatctatagcgcattagaaaataatggcaatattaacgcacctcactt"
+				+ "attaaaagacacgaaaaacaaagtttggaagaaaaatattatttccaaagaaaata"
+				+ "tcaatctattaactgatggtatgcaacaagtcgtaaataaaacacataaagaagat"
+				+ "atttatagatcttatgca";
+		// MRSA strain MSTC7 mecA gene sequence
+		String MSTC7 = "tggggtggttacaacgttacaagatatgaagtggtaaatggtaatatcgacttaa"
+				+ "aacaagcaatagaatcatcagataacattttctttgctagagtagcactcgaatta"
+				+ "ggcagtaagaaatttgaaaaaggcatgaaaaaactaggtgttggtgaagatatacc"
+				+ "aagtgattatccattttataatgctcaaatttcaaacaaaaatttagataatgaaa"
+				+ "tattattagctgattcaggttacggacaaggtgaaatactgattaacccagtacag"
+				+ "atcctttcaatctatagcgcattagaaaataatggcaatattaacgcacctcactt"
+				+ "attaaaagacacgaaaaacaaagtttggaagaaaaatattatttccaaagaaaata"
+				+ "tcaatctattaactgatggtatgcaacaagtcgtaaataaaacacataaagaag";
+		int N = MSTB8.length();
+		int M = MSTC7.length();
 		
 		// Scoring matrix s 
 		double[][] s = new double[N][M];
@@ -32,15 +49,16 @@ public class DynamicXDrop {
 				double j = k-i; 
 				
 				if ((i+half < N) && (j+half < M)) {
+					// Do this if values are not halves 
 					if (i == (int) i) {
 						double s1 = Double.NEGATIVE_INFINITY;
 						double s2 = Double.NEGATIVE_INFINITY;
 						double s3 = Double.NEGATIVE_INFINITY;
 						double s4 = Double.NEGATIVE_INFINITY;					
 						
-						if (L <= i-half && U > i-half && a.charAt((int) i) == (b.charAt((int) j))) {
+						if (L <= i-half && U > i-half && MSTB8.charAt((int) i) == (MSTC7.charAt((int) j))) {
 							s1 = s[(int) (i-half)][(int) (j-half)] + (mat/2);
-						} if (L <= i-half && U > i-half && a.charAt((int) i) != (b.charAt((int) j))) {
+						} if (L <= i-half && U > i-half && MSTB8.charAt((int) i) != (MSTC7.charAt((int) j))) {
 							s2 = s[(int) (i-half)][(int) (j-half)] + (mis/2);
 						} if (i <= U) {
 							s3 = s[(int) i][(int) (j-half)] + ind;
@@ -49,12 +67,12 @@ public class DynamicXDrop {
 						}
 		
 						s[(int) i][(int) j] = Math.max(s1, Math.max(s2, Math.max(s3, s4)));
-						
+					// Do this if values are half values
 					} else {
 						double score = 0;
-						if (a.charAt((int) (i+half)) == b.charAt((int) (j+half))) {
+						if (MSTB8.charAt((int) (i+half)) == MSTC7.charAt((int) (j+half))) {
 							score = mat/2;
-						} else if (a.charAt((int) (i+half)) != b.charAt((int) (j+half))) {
+						} else if (MSTB8.charAt((int) (i+half)) != MSTC7.charAt((int) (j+half))) {
 							score = mis/2;
 						}
 						s[(int) i][(int) j] = s[(int) (i-half)][(int) (j-half)] + score;
