4aeb1849661d5e623056fe572fb68306ab37e568
reneelau <reneel4u@gmail.com>
2017-03-31 13:36:52 -0700

    fixed errors in 76-83

--- src/DynamicXDrop.java
+++ src/DynamicXDrop.java
@@ -56,13 +56,13 @@ public class DynamicXDrop {
 		// Scoring matrix s 
 		double[][] s = new double[N][M];
 		s[0][0] = 0;
-		
 
 		long startTime = System.nanoTime();
 		
+		double i = 0;
 		while (L <= U+1) {
 			k++;
-			for (double i = Math.ceil(L); i <= Math.floor(U)+1; i = i + half) {
+			for (i = Math.ceil(L); i <= Math.floor(U)+1; i = i + half) {
 				double j = k-i; 
 				
 				if ((i+half < N) && (j+half < M)) {
@@ -73,14 +73,14 @@ public class DynamicXDrop {
 						double s3 = Double.NEGATIVE_INFINITY;
 						double s4 = Double.NEGATIVE_INFINITY;					
 						
-						if (L <= i-half && U > i-half && MSTB8.charAt((int) i) == (MSTC7.charAt((int) j))) {
+						if (L <= i-half && U >= i-half && MSTB8.charAt((int) i) == (MSTC7.charAt((int) j))) {
 							s1 = s[(int) (i-half)][(int) (j-half)] + (mat/2);
-						} if (L <= i-half && U > i-half && MSTB8.charAt((int) i) != (MSTC7.charAt((int) j))) {
+						} if (L <= i-half && U >= i-half && MSTB8.charAt((int) i) != (MSTC7.charAt((int) j))) {
 							s2 = s[(int) (i-half)][(int) (j-half)] + (mis/2);
 						} if (i <= U) {
-							s3 = s[(int) i][(int) (j-half)] + ind;
+							s3 = s[(int) i][(int) (j-1)] + ind;
 						} if (L <= i-1) {
-							s4 = s[(int) (i-half)][(int) j] + ind;
+							s4 = s[(int) (i-1)][(int) j] + ind;
 						}
 		
 						s[(int) i][(int) j] = Math.max(s1, Math.max(s2, Math.max(s3, s4)));
